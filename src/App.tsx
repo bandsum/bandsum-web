437bb52804e7139483dcf1a71c9b9049a38b46e3
nkEllaaa <dhrtns515@gmail.com>
2023-11-30 23:08:35 +0900

    feat: 앱에서 스플래쉬와 내 주변 합주실의 라우트 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import GlobalStyle from './style/globalStyle';
+import Splash from './pages/Splash';
+import EnsembleRoomAroundMe from './pages/EnsembleRoomAroundMe';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <></>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/splash" element={<Splash />} />
+        <Route path="/main" element={<EnsembleRoomAroundMe />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
